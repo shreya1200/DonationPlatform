5d2f2157736c247db69711c1b249442112fda8c3
Shreya_12 <shreya.gore2000@gmail.com>
2020-09-29 12:02:20 +0530

    ui till categories done

---
+++ Donation/app/src/main/java/com/example/donation/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.donation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;//transition from one activity to another after a specific interval of time
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    Timer timer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,homepage.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
